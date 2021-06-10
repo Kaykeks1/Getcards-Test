66fcf0a06d46d6667f6cc0a90d1e590c4917b589
Kaykeks1 <kayuskeks@gmail.com>
2021-06-10 22:55:56 +0100

    added html. added npm start script

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const resolvers = require('./resolvers');
 const schema = require('./schema');
-
+const path = require('path');
 
 function createServer() {
   const app = express();
   const { graphqlHTTP } = require('express-graphql');
   const root = resolvers;
+
+  app.use(express.static(__dirname + ''));
+  app.use(express.static(path.join(__dirname, '')));
+
+  app.get("/", function (request, response){
+    //show this file when the "/" is requested
+    response.sendFile(__dirname+"/index.html");
+  });
 
   app.use('/graphiql', graphqlHTTP({
     schema: schema,
